c4b3f87f5520802dd50c87d15d75a5f3970f2bb2
cobid <cobi.mail.d@gmail.com>
2022-05-18 17:03:46 +0300

    po1

---
+++ src/P01BrowserHistory.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String currentUrl = " ";
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (!arrayDeque.isEmpty()) {
+                    currentUrl = arrayDeque.pop();
+                }else {
+                    System.out.println("no previous URLs");
+                    input = scanner.nextLine();
+                    continue;
+                }
+            } else {
+                if (!currentUrl.equals(" ")) {
+                    arrayDeque.push(currentUrl);
+                }
+                currentUrl = input;
+            }
+            System.out.println(currentUrl);
+            input = scanner.nextLine();
+        }
+    }
+}
+
