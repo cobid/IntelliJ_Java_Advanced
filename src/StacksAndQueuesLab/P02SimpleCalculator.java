2c1d708be0b0e916d7c83539a7fa5a3249e29a63
cobid <cobi.mail.d@gmail.com>
2022-05-18 20:08:53 +0300

    package

--- src/StacksAndQueuesLab/P02SimpleCalculator.java
+++ src/StacksAndQueuesLab/P02SimpleCalculator.java
@@ -3,7 +3,7 @@ package StacksAndQueuesLab;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class P2SimpleCalculator {
+public class P02SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] input = scanner.nextLine().split("\\s+");
@@ -15,7 +15,7 @@ public class P2SimpleCalculator {
             int topNum = Integer.parseInt(stack.pop());
             String operator = stack.pop();
             int secondFromTopNum = Integer.parseInt(stack.pop());
-            int result = 0;
+            int result;
             if (operator.equals("+")) {
                 result = topNum + secondFromTopNum;
             } else {
