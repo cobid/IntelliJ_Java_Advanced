83a9f6224689cd4192c5ed2984a03c5db32a7981
cobid <cobi.mail.d@gmail.com>
2022-06-05 15:16:26 +0300

    package

---
+++ src/FunctionalProgrammingExercises/p1ConsumerPrint.java
@@ -0,0 +1,16 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class p1ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> input = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        Consumer<String> consumer = System.out::println;
+        input.forEach(consumer);
+    }
+}
