124bd82d497701c0d16a6738c24b4d96a84d0d6c
cobid <cobi.mail.d@gmail.com>
2022-06-13 14:36:49 +0300

    package

---
+++ src/_3_DefiningClassesSpeedRacing/Car.java
@@ -0,0 +1,58 @@
+package _3_DefiningClassesSpeedRacing;
+
+public class Car {
+    private String carModel;
+    private double fuelVolume;
+    private double fuelCostPerKm;
+    private int amountOfKm;
+
+    public Car(String carModel, double fuelVolume, double fuelCostPerKm) {
+        this.carModel = carModel;
+        this.fuelVolume = fuelVolume;
+        this.fuelCostPerKm = fuelCostPerKm;
+        this.amountOfKm = 0;
+    }
+
+    public int getAmountOfKm() {
+        return amountOfKm;
+    }
+
+    public void setAmountOfKm(int amountOfKm) {
+        this.amountOfKm = amountOfKm;
+    }
+
+    public double getFuelVolume() {
+        return fuelVolume;
+    }
+
+    public void setFuelVolume(double fuelVolume) {
+        this.fuelVolume = fuelVolume;
+    }
+
+    public double getFuelCostPerKm() {
+        return fuelCostPerKm;
+    }
+
+    public void setFuelCostPerKm(double fuelCostPerKm) {
+        this.fuelCostPerKm = fuelCostPerKm;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void carConsume(int amountOfKm) {
+        if(this.fuelCostPerKm * amountOfKm <=  this.fuelVolume) {
+            this.setFuelVolume(this.fuelVolume - (this.fuelCostPerKm * amountOfKm));
+            this.setAmountOfKm(this.amountOfKm + amountOfKm);
+        } else {
+            System.out.println("Insufficient fuel for the drive");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d", this.carModel, this.fuelVolume, this.amountOfKm);
+    }
+
+}
