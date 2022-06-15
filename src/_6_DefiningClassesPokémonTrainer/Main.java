5d66e53e1eedd233867471fb157ab62de76cd8e1
cobid <cobi.mail.d@gmail.com>
2022-06-15 22:30:30 +0300

    package

---
+++ src/_6_DefiningClassesPokémonTrainer/Main.java
@@ -0,0 +1,60 @@
+package _6_DefiningClassesPokémonTrainer;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Trainer> trainerList = new ArrayList<>();
+        Pokemon pokemon;
+
+        String input = scanner.nextLine();
+        while (!input.equals("Tournament")) {
+            String[] inputData = input.split("\\s+");
+            //{TrainerName} {PokemonName} {PokemonElement} {PokemonHealth}
+            String trainerName = inputData[0];
+            String pokemonName = inputData[1];
+            String pokemonElement = inputData[2];
+            int pokemonHealth = Integer.parseInt(inputData[3]);
+            pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+            if (trainerList.stream().noneMatch(tr -> Objects.equals(tr.getName(), trainerName))) {
+                Trainer trainer = new Trainer(trainerName);
+                trainerList.add(trainer);
+                trainer.getCollectionOfPokemon().add(pokemon);
+            } else {
+                trainerList.stream().filter(tr -> Objects.equals(tr.getName(), trainerName))
+                        .collect(Collectors.toList()).get(0)
+                        .getCollectionOfPokemon().add(pokemon);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        String inputCommands = scanner.nextLine();
+        while (!inputCommands.equals("End")) {
+            for (Trainer trainer : trainerList) {
+                String finalInputCommands = inputCommands;
+                if (trainer.getCollectionOfPokemon().size() > 0) {
+                    if (trainer.getCollectionOfPokemon().stream().anyMatch(pok -> pok.getElement().equals(finalInputCommands))) {
+                        trainer.setNumberOfBadges(trainer.getNumberOfBadges() + 1);
+                    } else {
+                        for (Pokemon item : trainer.getCollectionOfPokemon()) {
+                            item.setHealth(item.getHealth() - 10);
+                        }
+                        trainer.setCollectionOfPokemon(trainer.getCollectionOfPokemon()
+                                .stream().filter(e -> e.getHealth() > 0)
+                                .collect(Collectors.toList()));
+                    }
+                }
+            }
+            inputCommands = scanner.nextLine();
+        }
+        for (Trainer trainer : trainerList.stream()
+                .sorted((e1, e2) -> Integer.compare(e2.getNumberOfBadges(), e1.getNumberOfBadges()))
+                .collect(Collectors.toList())) {
+            System.out.println(trainer.toString());
+        }
+    }
+}
---
+++ src/_6_DefiningClassesPokémonTrainer/Trainer.java
@@ -0,0 +1,52 @@
+package _6_DefiningClassesPokémonTrainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trainer {
+    //name, a number of badges, and a collection of pokemon
+    String name;
+    int numberOfBadges;
+    List<Pokemon> pokemonsList;
+
+
+    public Trainer(String name) {
+        this.name = name;
+        this.numberOfBadges = 0;
+        this.pokemonsList = new ArrayList<>();
+    }
+
+    public Trainer() {
+        this.numberOfBadges = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfBadges() {
+        return numberOfBadges;
+    }
+
+    public void setNumberOfBadges(int numberOfBadges) {
+        this.numberOfBadges = numberOfBadges;
+    }
+
+    public List<Pokemon> getCollectionOfPokemon() {
+        return this.pokemonsList;
+    }
+
+    public void setCollectionOfPokemon(List<Pokemon> pokemonsList) {
+        this.pokemonsList = pokemonsList;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s %d %d", this.name, this.numberOfBadges, pokemonsList.size());
+    }
+}
